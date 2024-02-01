b19cbff58f02d7a93ef797bdec7df6276ec911e4
DenisPA <denispokhodnia@gmail.com>
2024-02-01 23:40:59 +0300

    The first interface update

--- BankCrypto/AutorizationWindow.cs
+++ BankCrypto/AutorizationWindow.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace BankCrypto
 {
-    public partial class Form1 : Form
+    public partial class AutorizationWindow : Form
     {
-        public Form1()
+        public AutorizationWindow()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
--- BankCrypto/Program.cs
+++ BankCrypto/Program.cs
@@ -16,7 +16,7 @@ namespace BankCrypto
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new AutorizationWindow());
         }
     }
 }
